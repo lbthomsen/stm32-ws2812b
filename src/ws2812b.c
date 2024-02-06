bd244e3c9ca2bd1b66e06223a58140a0cd6fb35d
Lars Boegild Thomsen <lth@netcompartner.com>
2024-02-06 17:50:56 +0800

    Tweakin f103 demo

--- src/ws2812b.c
+++ src/ws2812b.c
@@ -5,8 +5,8 @@
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; Copyright (c) 2022 Lars Boegild Thomsen <lbthomsen@gmail.com>.
- * All rights reserved.</center></h2>
+ * Copyright (c) 2022 Lars Boegild Thomsen <lbthomsen@gmail.com>.
+ * All rights reserved
  *
  * This software component is licensed by lbthomsen under MIT license,
  * the "License"; You may not use this file except in compliance with the
@@ -67,61 +67,61 @@ static inline void update_next_buffer() {
 	HAL_GPIO_WritePin(BUFF_GPIO_Port, BUFF_Pin, GPIO_PIN_SET);
 #endif
 
-	// A simple state machine - we're either resetting (two buffers worth of zeros),
-	// idle (just winging out zero buffers) or
-	// we are transmitting data for the "current" led.
+    // A simple state machine - we're either resetting (two buffers worth of zeros),
+    // idle (just winging out zero buffers) or
+    // we are transmitting data for the "current" led.
 
-	if (led_state == LED_RES) { // Latch state - 10 or more full buffers of zeros
+    if (led_state == LED_RES) { // Latch state - 10 or more full buffers of zeros
 
-		// This one is simple - we got a bunch of zeros of the right size - just throw
-		// that into the buffer.  Twice will do (two half buffers).
-		if (zero_halves < 2) {
-			memset(dma_buffer_pointer, 0, 48); // Fill the buffer with zeros
-			zero_halves++; // We only need to update two half buffers
-		}
+        // This one is simple - we got a bunch of zeros of the right size - just throw
+        // that into the buffer.  Twice will do (two half buffers).
+        if (zero_halves < 2) {
+            memset(dma_buffer_pointer, 0, 48); // Fill the buffer with zeros
+            zero_halves++; // We only need to update two half buffers
+        }
 
-		res_cnt++;
+        res_cnt++;
 
-		if (res_cnt >= LED_RESET_CYCLES) { // done enough reset cycles - move to next state
-			led_cnt = 0;	// prepare to send data
-			if (is_dirty) {
-				is_dirty = false;
-				led_state = LED_DAT;
-			} else {
-				led_state = LED_IDL;
-			}
-		}
+        if (res_cnt >= LED_RESET_CYCLES) { // done enough reset cycles - move to next state
+            led_cnt = 0;	// prepare to send data
+            if (is_dirty) {
+                is_dirty = false;
+                led_state = LED_DAT;
+            } else {
+                led_state = LED_IDL;
+            }
+        }
 
-	} else if (led_state == LED_IDL) { // idle state
+    } else if (led_state == LED_IDL) { // idle state
 
-		if (is_dirty) { // we do nothing here except waiting for a dirty flag
-			is_dirty = false;
-			led_state = LED_DAT; // when dirty - start processing data
-		}
+        if (is_dirty) { // we do nothing here except waiting for a dirty flag
+            is_dirty = false;
+            led_state = LED_DAT; // when dirty - start processing data
+        }
 
-	} else { // LED state
+    } else { // LED state
 
-		// First let's deal with the current LED
-		uint8_t *led = (uint8_t*) &led_value[3 * led_cnt];
+        // First let's deal with the current LED
+        uint8_t *led = (uint8_t*) &led_value[3 * led_cnt];
 
-		for (uint8_t c = 0; c < 3; c++) { // Deal with the 3 color leds in one led package
+        for (uint8_t c = 0; c < 3; c++) { // Deal with the 3 color leds in one led package
 
-			// Copy values from the pre-filled color_value buffer
-			memcpy(dma_buffer_pointer, color_value[led[c]], 16); // Lookup the actual buffer data
-			dma_buffer_pointer += 8; // next 8 bytes
+            // Copy values from the pre-filled color_value buffer
+            memcpy(dma_buffer_pointer, color_value[led[c]], 16); // Lookup the actual buffer data
+            dma_buffer_pointer += 8; // next 8 bytes
 
-		}
+        }
 
-		// Now move to next LED switching to reset state when all leds have been updated
-		led_cnt++; // Next led
-		if (led_cnt >= leds) { // reached top
-			led_cnt = 0; // back to first
-			zero_halves = 0;
-			res_cnt = 0;
-			led_state = LED_RES;
-		}
+        // Now move to next LED switching to reset state when all leds have been updated
+        led_cnt++; // Next led
+        if (led_cnt >= leds) { // reached top
+            led_cnt = 0; // back to first
+            zero_halves = 0;
+            res_cnt = 0;
+            led_state = LED_RES;
+        }
 
-	}
+    }
 
 #ifdef BUFF_GPIO_Port
 	HAL_GPIO_WritePin(BUFF_GPIO_Port, BUFF_Pin, GPIO_PIN_RESET);
@@ -132,70 +132,69 @@ static inline void update_next_buffer() {
 // Done sending first half of the DMA buffer - this can now safely be updated
 void HAL_TIM_PWM_PulseFinishedHalfCpltCallback(TIM_HandleTypeDef *htim) {
 
-	if (htim->Instance == timer->Instance) {
-		dma_buffer_pointer = &dma_buffer[0];
-		update_next_buffer();
-	}
+    if (htim->Instance == timer->Instance) {
+        dma_buffer_pointer = &dma_buffer[0];
+        update_next_buffer();
+    }
 
 }
 
 // Done sending the second half of the DMA buffer - this can now be safely updated
 void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim) {
 
-	if (htim->Instance == timer->Instance) {
-		dma_buffer_pointer = &dma_buffer[BUFFER_SIZE];
-		update_next_buffer();
-	}
+    if (htim->Instance == timer->Instance) {
+        dma_buffer_pointer = &dma_buffer[BUFFER_SIZE];
+        update_next_buffer();
+    }
 
 }
 
 void zeroLedValues() {
-	memset(led_value, 0, leds * 3); // Zero it all
-	is_dirty = true; // Mark buffer dirty
+    memset(led_value, 0, leds * 3); // Zero it all
+    is_dirty = true; // Mark buffer dirty
 }
 
 void setLedValue(uint16_t led, uint8_t col, uint8_t value) {
-	if (led < leds) {
-		led_value[3 * led + col] = value;
-		is_dirty = true; // Mark buffer dirty
-	}
+    if (led < leds) {
+        led_value[3 * led + col] = value;
+        is_dirty = true; // Mark buffer dirty
+    }
 }
 
 // Just throw values into led_value array - the dma interrupt will
 // handle updating the dma buffer when needed
 void setLedValues(uint16_t led, uint8_t r, uint8_t g, uint8_t b) {
-	if (led < leds) {
-		led_value[3 * led + RL] = r;
-		led_value[3 * led + GL] = g;
-		led_value[3 * led + BL] = b;
-		is_dirty = true; // Mark buffer dirty
-	}
+    if (led < leds) {
+        led_value[3 * led + RL] = r;
+        led_value[3 * led + GL] = g;
+        led_value[3 * led + BL] = b;
+        is_dirty = true; // Mark buffer dirty
+    }
 }
 
-uint8_t ws2812b_init(TIM_HandleTypeDef *init_timer, uint32_t init_channel,
-		uint16_t init_leds) {
+uint8_t ws2812b_init(TIM_HandleTypeDef *init_timer, uint32_t init_channel, uint16_t init_leds) {
 
-	// Store timer handle for later
-	timer = init_timer;
+    // Store timer handle for later
+    timer = init_timer;
 
-	// Store channel
-	channel = init_channel;
+    // Store channel
+    channel = init_channel;
 
-	leds = init_leds;
+    leds = init_leds;
 
-	led_value = malloc(leds * 3);
-	if (led_value != NULL) { // Memory for led values
+    led_value = malloc(leds * 3);
+    if (led_value != NULL) { // Memory for led values
 
-		memset(led_value, 0, leds * 3); // Zero it all
+        memset(led_value, 0, leds * 3); // Zero it all
 
-		// Start DMA to feed the PWM with values
-		// At this point the buffer should be empty - all zeros
-		HAL_TIM_PWM_Start_DMA(timer, channel, (uint32_t*) dma_buffer,
-		BUFFER_SIZE * 2);
-		return WS2812B_INIT_OK;
-	} else {
-		return WS2812B_INIT_MEM;
-	}
+        // Start DMA to feed the PWM with values
+        // At this point the buffer should be empty - all zeros
+        HAL_TIM_PWM_Start_DMA(timer, channel, (uint32_t*) dma_buffer,
+        BUFFER_SIZE * 2);
+        return WS2812B_INIT_OK;
+    } else {
+        return WS2812B_INIT_MEM;
+    }
 
 }
 
--- src/ws2812b.h
+++ src/ws2812b.h
@@ -22,7 +22,7 @@
 // Buffer allocated will be twice this
 #define BUFFER_SIZE 24
 
-// LED on/off counts.  PWM timer is running 104 counts.
+// LED on/off counts.  PWM timer is running 104 counts.  LED_CNT need to be set to the total counts in the PWM.
 #define LED_OFF 1 * LED_CNT / 3 - 1
 #define LED_ON 2 * LED_CNT / 3 + 2
 #define LED_RESET_CYCLES 10 // Full 24-bit cycles
@@ -43,14 +43,13 @@ void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim);
 void HAL_TIM_PWM_PulseFinishedHalfCpltCallback(TIM_HandleTypeDef *htim);
 
 typedef struct {
-	TIM_HandleTypeDef *timer0;
-	TIM_HandleTypeDef *timer1;
-	uint32_t channel0;
-	uint32_t channel1;
+    TIM_HandleTypeDef *timer0;
+    TIM_HandleTypeDef *timer1;
+    uint32_t channel0;
+    uint32_t channel1;
 } ws2823b_init_TypeDef;
 
-uint8_t ws2812b_init(TIM_HandleTypeDef *init_timer, uint32_t init_channel,
-		uint16_t init_leds);
+uint8_t ws2812b_init(TIM_HandleTypeDef *init_timer, uint32_t init_channel, uint16_t init_leds);
 
 // Set all led values to zero
 void zeroLedValues();
