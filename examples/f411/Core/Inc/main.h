29cbfcda27ce1a2dd953a0707e5520a56f866869
Lars Boegild Thomsen <lth@netcompartner.com>
2024-02-08 09:43:48 +0800

    Updated headers

--- examples/f411/Core/Inc/main.h
+++ examples/f411/Core/Inc/main.h
@@ -1,21 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.h
-  * @brief          : Header for main.c file.
-  *                   This file contains the common defines of the application.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2022 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.h
+ * @brief          : Main program header
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 Lars Boegild Thomsen <lbthomsen@gmail.com>.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
